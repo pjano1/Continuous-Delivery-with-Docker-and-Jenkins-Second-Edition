248373b2b9112d80425280d8e1421f1620629aa0
pjano1 <47459720+pjano1@users.noreply.github.com>
2022-03-30 18:42:45 -0400

    Adds division to StepDefinitions.java

--- Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
+++ Chapter09/sample3/src/test/java/acceptance/StepDefinitions.java
@@ -17,6 +17,10 @@ public class StepDefinitions {
     private String b;
     private String result;
 
+    private String c;
+    private String d;
+    private String div_result;
+
     @Given("^I have two numbers: (.*) and (.*)$")
     public void i_have_two_numbers(String a, String b) throws Throwable {
         this.a = a;
@@ -32,5 +36,22 @@ public class StepDefinitions {
     @Then("^I receive (.*) as a result$")
     public void i_receive_as_a_result(String expectedResult) throws Throwable {
         assertEquals(expectedResult, result);
+    }
+
+    @Given("^I have dividend and divisor: (.*) and (.*)$")
+    public void divide_two_numbers(String c, String d) throws Throwable {
+        this.c = c;
+        this.d = d;
+    }
+
+    @When("^the calculator divides them$")
+    public void the_calculator_sums_them() throws Throwable {
+        String url = String.format("%s/div?a=%s&b=%s", server, c, d);
+        result = restTemplate.getForObject(url, String.class);
+    }
+
+    @Then("^I receive (.*) as the quotient$")
+    public void quotient_result(String expectedResult) throws Throwable {
+        assertEquals(expectedResult, div_result);
     }
 }
